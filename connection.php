38e9f8712ab41e0b8e9646b61ca7dc5415f3cd7c
NKHN <b5017063@my.shu.ac.uk>
2017-01-10 10:31:22 +0000

    PHP Files

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=homepages.shu.ac.uk;dbname=b5017063_db1';
+$user = 'b5017063';
+$password = 'mysql123';
+try { 
+$pdo = new PDO($dsn, $user, $password); 
+$pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$pdo ->exec("SET CHARACTER SET utf8");
+}
+catch (PDOException $e) { 
+echo 'Connection failed again: ' . $e->getMessage();
+}
+?>
